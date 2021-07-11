550efe2cc30295413f6a56fbb71e4cf9efa55e81
WebCoderD <78393605+WebCoderD@users.noreply.github.com>
2021-07-11 15:05:15 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+    const divs = document.querySelector('.row > div');
+
+  function showDiv() {
+    this.classList.toggle('show-description');
+  }
+
+  divs.addEventListener('click', showDiv)
+
+  const div = document.querySelector('.bad ');
+
+  function showDi() {
+    this.classList.toggle('show-description');
+  }
+
+  div.addEventListener('click', showDi)
+
+
+  const di = document.querySelector('.ugly');
+
+  function showDv() {
+    this.classList.toggle('show-description');
+  }
+
+  di.addEventListener('click', showDv)
+
